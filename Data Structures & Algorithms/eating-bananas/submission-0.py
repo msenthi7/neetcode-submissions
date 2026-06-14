0469b111a24082e3185da3fb8d29c237e599a295
msenthi7 <matheshwara13@gmail.com>
2026-06-13 23:20:39 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,21 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l=1
+        r=max(piles)
+        while l<r:
+            thrs=0
+            mid=l+(r-l)//2
+            for i in piles:
+                thrs+=math.ceil(i/mid)
+            if thrs<=h:
+                r=mid
+            elif thrs>h:
+                l=mid+1
+            else:
+                return l
+        return l
+
+
+
+        
